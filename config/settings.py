8ed492120417412a9c63757a857fec6d97ab7b8a
dai0105 <daiki4457@gmail.com>
2026-03-25 16:17:41 +0900

    Update settings and environment config

--- config/settings.py
+++ config/settings.py
@@ -82,11 +82,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-
 DATABASES = {
     'default': dj_database_url.config(
-        default="postgresql://board_db_ufvt_user:Uy53T8xk45v6AKwmFUCs5XO2qWVW4pJw@dpg-d6vbrl7afjfc73cuobg0-a/board_db_ufvt",
         conn_max_age=600,
+        ssl_require=True,
     )
 }
 
